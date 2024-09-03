6e458989fdb6d0162582583ece43e9cf8e54e440
Chris <atnl@protonmail.com>
2024-09-03 19:36:19 -0400

    Update options.lua

--- lua/spL1n73r/core/options.lua
+++ lua/spL1n73r/core/options.lua
@@ -39,4 +39,3 @@ opt.splitbelow = true
 
 vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, { "ExtraGroup" })
 -- vimscript: let g:transparent_groups = extend(get(g:, 'transparent_groups', []), ["ExtraGroup"])
-kkj
