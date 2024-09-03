fa4483113a820d311a384145161d1adabc58bcdc
Chris <atnl@protonmail.com>
2024-09-03 19:23:19 -0400

    Update options.lua

--- lua/spL1n73r/core/options.lua
+++ lua/spL1n73r/core/options.lua
@@ -6,11 +6,12 @@ opt.number = true
 -- Tabs & Indents
 opt.autoindent = true
 opt.expandtab = true
-opt.tabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.shiftwidth = 4
 
 opt.wrap = false
 opt.cursorline = true
+opt.colorcolumn = "80"
 
 -- Colorscheme options
 opt.termguicolors = true
@@ -23,9 +24,19 @@ opt.backspace = "indent,eol,start"
 -- Clipboard
 opt.clipboard:append("unnamedplus")
 
+-- Changes/memory
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.scrolloff = 8
+opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
 -- Split Windows
 opt.splitright = true
 opt.splitbelow = true
 
 vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, { "ExtraGroup" })
 -- vimscript: let g:transparent_groups = extend(get(g:, 'transparent_groups', []), ["ExtraGroup"])
+kkj
