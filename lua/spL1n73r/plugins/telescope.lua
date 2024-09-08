ba100cf64549cbd67ff96d934481c0322591717f
spL1n73r-xx <spL1n73r@proton.me>
2024-09-08 15:04:01 -0400

    Update telescope.lua

--- lua/spL1n73r/plugins/telescope.lua
+++ lua/spL1n73r/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release"
 		},
 		"nvim-tree/nvim-web-devicons",
 	},
