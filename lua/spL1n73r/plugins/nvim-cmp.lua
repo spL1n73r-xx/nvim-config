b1529c18b72530baa2407dc231760ac3aa7c4ce0
spL1n73r-xx <spL1n73r@proton.me>
2024-09-08 15:08:57 -0400

    Update nvim-cmp.lua

--- lua/spL1n73r/plugins/nvim-cmp.lua
+++ lua/spL1n73r/plugins/nvim-cmp.lua
@@ -7,7 +7,7 @@ return {
     {
       "L3MON4D3/LuaSnip",
       version = "v2.*",
-      build = "make install_jsregexp",
+      --build = "make install_jsregexp",
     },
     "saadparwaiz1/cmp_luasnip",
     "rafamadriz/friendly-snippets",
